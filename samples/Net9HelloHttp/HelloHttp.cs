ff5253c070499a2ac8da0674e04fbd89885e3e97
Shyju Krishnankutty <connectshyju@gmail.com>
2024-06-07 14:33:51 -0700

    NET 9 FNH, Prelaunch app, Sample app

---
+++ samples/Net9HelloHttp/HelloHttp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Net9HelloHttp
+{
+    public class HelloHttp
+    {
+        private readonly ILogger<HelloHttp> _logger;
+
+        public HelloHttp(ILogger<HelloHttp> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("hellohttp")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {       
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Net9 Azure Functions!");
+        }
+    }
+}
